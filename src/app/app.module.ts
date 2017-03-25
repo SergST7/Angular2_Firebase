ca728e1472dcaee3a4fa8fbd6f771daf399926e0
Sergey Stelmakh <stelmakhserg@gmail.com>
2017-03-25 23:58:21 +0200

    added FireBase config and initialize FireBase app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {AngularFireModule} from "angularfire2";
 
 import {AppRouting} from "./app.routing";
 
@@ -12,6 +13,15 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { AddListingComponent } from './components/add-listing/add-listing.component';
 import { EditListingComponent } from './components/edit-listing/edit-listing.component';
 import { ListListingComponent } from './components/list-listing/list-listing.component';
+
+
+export const fbConfig = {
+  apiKey: "AIzaSyBq8h1b0GlLcVstcOr0IdBfrXj4bek5OmQ",
+  authDomain: "property-listing-c4d57.firebaseapp.com",
+  databaseURL: "https://property-listing-c4d57.firebaseio.com",
+  storageBucket: "property-listing-c4d57.appspot.com",
+  messagingSenderId: "484585922588"
+};
 
 
 @NgModule({
@@ -28,7 +38,8 @@ import { ListListingComponent } from './components/list-listing/list-listing.com
     BrowserModule,
     FormsModule,
     HttpModule,
-    AppRouting
+    AppRouting,
+    AngularFireModule.initializeApp(fbConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
