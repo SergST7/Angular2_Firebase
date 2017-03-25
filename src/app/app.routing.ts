f73acae79c18462e0cdec36524819a3656180916
Sergey Stelmakh <stelmakhserg@gmail.com>
2017-03-25 23:51:34 +0200

    added routing to addListing Component

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,12 +5,14 @@
 import {Routes, RouterModule} from "@angular/router";
 import {HomeComponent} from "./components/home/home.component";
 import {ListListingComponent} from "./components/list-listing/list-listing.component";
+import {AddListingComponent} from "./components/add-listing/add-listing.component";
 
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: "full"},
   {path: 'home', component: HomeComponent},
   {path: 'listings', component: ListListingComponent},
+  {path: 'add-listings', component: AddListingComponent},
 ];
 
 export const AppRouting = RouterModule.forRoot(routes);
