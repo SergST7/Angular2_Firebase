54759a9797ec03067423b05f8efc6273ed98b946
Sergey Stelmakh <stelmakhserg@gmail.com>
2017-03-27 23:19:53 +0300

    added func in Firebase services for adding images in fb storage, and save new listing in firebase

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import {AngularFire, FirebaseListObservable, FirebaseObjectObservable} from "angularfire2";
+import * as firebase from 'firebase'
+
 
 interface Listing{
   $key?:string;
@@ -15,8 +17,11 @@ interface Listing{
 export class FirebaseService {
   listings: FirebaseListObservable<any[]>;
   listing: FirebaseObjectObservable<any>;
+  folder: any;
 
-  constructor(private af: AngularFire, ) { }
+  constructor(private af: AngularFire, ) {
+    this.folder = 'listing-images'
+  }
 
   getListings(){
     this.listings = this.af.database.list('/listings') as FirebaseListObservable<Listing[]>;
@@ -26,6 +31,21 @@ export class FirebaseService {
   getListingDetail(id){
     this.listing = this.af.database.object('/listings/'+id) as FirebaseObjectObservable<Listing>;
     return this.listing
+  }
+
+  addListing(listing){
+    //create ref for firebase storage
+    let storageRef = firebase.storage().ref();
+    for(let fileSelected of [(<HTMLInputElement>document.getElementById('image')).files[0]]){
+      let path = `/${this.folder}/${fileSelected.name}`;
+      let imageRef = storageRef.child(path);
+      imageRef.put(fileSelected).then(snapshot => {
+        listing.image = fileSelected.name;
+        listing.path = path;
+
+        return this.listings.push(listing)
+      })
+    }
   }
 }
 
