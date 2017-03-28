fbc31c14d04575cf2145c742c17e61a958641b2a
Sergey Stelmakh <stelmakhserg@gmail.com>
2017-03-28 23:14:13 +0300

    describe editListing component class

--- src/app/components/edit-listing/edit-listing.component.ts
+++ src/app/components/edit-listing/edit-listing.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FirebaseService} from "../../services/firebase.service";
+import {Router, ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-edit-listing',
@@ -6,10 +8,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-listing.component.css']
 })
 export class EditListingComponent implements OnInit {
+  id: string;
+  title: any;
+  city: any;
+  owner: any;
+  bedrooms: any;
+  type: any;
+  price: any;
 
-  constructor() { }
+  constructor(
+    private fbService: FirebaseService,
+    private router: Router,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.id = this.route.snapshot.params['id'];
+
+    this.fbService.getListingDetail(this.id).subscribe(item => {
+        this.title = item.title;
+        this.city = item.city;
+        this.owner = item.owner;
+        this.bedrooms = item.bedrooms;
+        this.type = item.type;
+        this.price = item.price;
+    })
   }
+
+  onEditSubmit(){
+    let listing = {
+      title: this.title,
+      city:this.city,
+      owner: this.owner,
+      bedrooms: this.bedrooms,
+      type: this.type,
+      price: this.price
+    }
+
+    this.fbService.updateListing(this.id, listing){
+
+    }
+
+    this.router.navigate(['/listings'])
+  }
+
+
 
 }
